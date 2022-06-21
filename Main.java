edc13fce2332bb3a0b88f85fa4825ec05f7f1da1
Bartłomiej Łysik <100691680+Bartek2134@users.noreply.github.com>
2022-06-21 11:56:17 +0000

    Zaliczenie

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+ int[] tablica = {1,2,2,3,4};
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = i+1; j < 5; j++)
+            {
+                if(tablica[i] == tablica[j])
+                System.out.println(tablica [i]);
+            }
+        }
+      
   }
 }
